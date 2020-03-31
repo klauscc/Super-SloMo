6244d927329bd0a677aa4e37328826039d85cf0c
chengfeng <klaus.cheng@qq.com>
2020-03-31 18:58:09 -0400

    optimize workspace structure

---
+++ scripts/train.sh
@@ -0,0 +1,14 @@
+#================================================================
+#   God Bless You. 
+#   
+#   author: klaus
+#   email: chengfeng2333@gmail.com
+#   created date: 2020/03/31
+#   description: 
+#
+#================================================================
+
+python train.py  \
+    --dataset_root ~/stor6/dataset/processed_dataset/video-interpolation/adobe240fps \
+    --checkpoint_dir ~/stor/workspace/0331-video-interpolation/superSlowMo \
+    --epochs 200 \
